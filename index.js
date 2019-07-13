3ad709f3b28b73a021d954bf95af12951926cfe4
weerawat <weerewee1122@gmail.com>
2019-07-13 14:14:58 +0700

    เพิ่มหน้าใหม่แล้วนะ

--- index.js
+++ index.js
@@ -3,5 +3,9 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/contack', (req, res) => res.send('contack us'))
+app.get('/test', (req, res) => {
+    res.json({message:"test"})
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
